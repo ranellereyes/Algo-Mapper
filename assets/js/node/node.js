b7a4dc80d297c23d777dcc9d4312d944f54fa0b3
Winber Xu <winber1@gmail.com>
2017-06-05 18:02:48 -0700

    node class and astar constructor

--- assets/js/node/node.js
+++ assets/js/node/node.js
@@ -1,7 +1,7 @@
 class Node {
-  constructor(parentId, childId, weight, x, y) {
+  constructor(parentId, children, x, y) {
     this.parentId = parentId;
-    this.childId = childId;
+    this.childId = children;
     this.weight = weight;
     this.x = x;
     this.y = y;
@@ -9,5 +9,46 @@ class Node {
 }
 
 const NODELIST = {
-  1: new Node(undefined)
+  1: new Node([{ id: 2, weight: 10 }, { id: 3, weight: 5 }], 20, 20),
+  2: new Node([{ id: 5, weight: 8 }], 40, 10),
+  3: new Node([{ id: 5, weight: 5 }, { id: 4, weight: 7 }], 40, 30),
+  4: new Node([{ id: 6, weight: 9 }], 60, 30),
+  5: new Node([{ id: 6, weight: 2 }], 60, 10),
+  6: new Node([{}], 80, 20)
+}
+
+class Astar {
+  constructor() {
+    this.openList = [];
+    this.closeList = [];
+    this.path = [];
+  }
+
+  search(nodeList, startNodeId, endNodeId) {
+    let list = nodeList;
+    for (let idx in list) {
+      list[idx].f = 0;
+      list[idx].g = 0;
+      list[idx].h = 0;
+    }
+    this.openList.push(list[startNodeId]);
+
+
+    while (this.openList.length > 0) {
+      this.closeList.push(this.openList[0]);
+      this.openList = this.openList.slice(1);
+
+      let lowIdx = 0;
+      for (var i = 0; i < this.openList.length; i++) {
+        if (this.openList[i].f < this.openList[lowIdx].f) { lowIdx = i }
+      }
+      let current = this.openList[lowIdx];
+
+    }
+    return []
+  }
+
+  nearbyNodes() {
+
+  }
 }
