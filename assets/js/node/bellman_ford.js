71c915178890896cf3ab8f3565171dd42bcd05c1
Janice Yura Lee <JuraAir@JuraAir.local>
2017-06-06 11:30:51 -0700

    Start implementing bellmanford algorithm

---
+++ assets/js/node/bellman_ford.js
@@ -0,0 +1,41 @@
+// export class Node {
+//   constructor(children, x, y, id) {
+//     this.children = children;
+//     this.x = x;
+//     this.y = y;
+//     this.id = id;
+//   }
+// }
+// export const NODELIST = {
+//   1: new Node([{ id: 2, weight: 10 }, { id: 3, weight: 5 }], 20, 20, 1),
+//   2: new Node([{ id: 5, weight: 4 }], 40, 10, 2),
+//   3: new Node([{ id: 5, weight: 5 }, { id: 4, weight: 7 }], 40, 30, 3),
+//   4: new Node([{ id: 6, weight: 9 }], 60, 30, 4),
+//   5: new Node([{ id: 6, weight: 2 }], 60, 10, 5),
+//   6: new Node([{}], 80, 20, 6)
+// }
+
+class BellmanFord {
+  constructor(nodeList) {
+    this.openList = [];
+    this.closeList = [];
+    this.path = [];
+    this.nodeList = nodeList;
+    this.edgeList = this.createEdgeList(this.nodeList).bind(this);
+  }
+
+  search(startNodeId, endNodeId) {
+  }
+
+  createEdgeList(nodeList) {
+    let edges = [];
+    Object.keys(nodeList).forEach((nodeId) => {
+      nodeList[nodeId].children.forEach((child) => {
+        edges.push({ fromId: nodeId, toId: child.id, weight: child.weight});
+      });
+    });
+    return edges;
+  }
+}
+
+export default BellmanFord;
