0b4e634ddac152a8db8c0fd24da2fa370b234818
Winber Xu <winber1@gmail.com>
2017-06-05 17:13:34 -0700

    add webpack-stats.json to gitignore

--- assets/js/node/node.js
+++ assets/js/node/node.js
@@ -2,7 +2,12 @@ class Node {
   constructor(parentId, childId, weight, x, y) {
     this.parentId = parentId;
     this.childId = childId;
-
-    
+    this.weight = weight;
+    this.x = x;
+    this.y = y;
   }
+}
+
+const NODELIST = {
+  1: new Node(undefined)
 }
