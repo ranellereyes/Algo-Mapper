ca2e930378faea7c6eca93c743db91374d896ebb
Winber Xu <winber1@gmail.com>
2017-06-07 19:17:09 -0700

    can animate link

--- assets/js/d3/visualization.js
+++ assets/js/d3/visualization.js
@@ -1,3 +1,12 @@
+d3.selection.prototype.moveToBack = function() {
+  return this.each(function() {
+      var firstChild = this.parentNode.firstChild;
+      if (firstChild) {
+          this.parentNode.insertBefore(this, firstChild);
+      }
+  });
+};
+
 class Visualization {
   constructor(nodeList) {
     this.nodeList = nodeList;
@@ -23,28 +32,28 @@ class Visualization {
     return graph;
   }
 
-  centerTextX(x1, x2, y1, y2) {
+  centerTextX(x1, x2, y1, y2, weight) {
     let x = (x1 + x2) / 2;
     let dx = x2 - x1;
     let dy = y2 - y1;
     let radians = Math.atan(dy/dx);
-    if (Math.cos(radians) === 0) return x + 10;
+    if (Math.cos(radians) === 0) return x + weight;
     if ((dy > 0 && dx > 0) || (dy < 0 && dx > 0)) {
-      return x + Math.cos(radians) * 10;
+      return x + Math.cos(radians) * weight;
     }
-    return x - Math.cos(radians) * 10;
+    return x - Math.cos(radians) * weight;
   }
 
-  centerTextY(x1, x2, y1, y2) {
+  centerTextY(x1, x2, y1, y2, weight) {
     let y = (y1 + y2) / 2;
     let dx = x2 - x1;
     let dy = y2 - y1;
     let radians = Math.atan(dy/dx);
-    if (Math.sin(radians) === 0) return y + 10;
+    if (Math.sin(radians) === 0) return y + weight;
     if ((dy > 0 && dx > 0) || (dy < 0 && dx > 0)) {
-      return y - Math.sin(radians) * 10;
+      return y - Math.sin(radians) * weight;
     }
-    return y + Math.sin(radians) * 10;
+    return y + Math.sin(radians) * weight;
   }
 
   draw() {
@@ -56,7 +65,7 @@ class Visualization {
         .attr("height", 500);
 
     this.svg.append("defs").selectAll("marker")
-        .data(["base", "color", "color2"])
+        .data(["base", "color", "arrow"])
         .enter().append("marker")
         .attr("id", function(d) { return d })
         .attr("viewBox", "0 -5 10 10")
@@ -73,7 +82,11 @@ class Visualization {
 
     this.basePath = d3.select('marker#base path');
     this.colorPath = d3.select('marker#color path');
-    this.colorPath2 = d3.select('marker#color2 path');
+    this.arrowPath = d3.select('marker#arrow path');
+    this.arrowPath
+      .style("opacity", "0")
+      .style("stroke", "crimson")
+      .style("fill", "crimson");
 
     this.nodeGroup = this.svg.selectAll("g")
       .data(graph.nodes);
@@ -91,20 +104,23 @@ class Visualization {
         .style("stroke-width", 3)
         .style("marker-end",  "url(#base)");
 
+    this.bezierLine = d3.line()
+            .x(function(d) { return d[0]; })
+            .y(function(d) { return d[1]; })
+            .curve(d3.curveBundle.beta(0.5));
+
     this.svg.selectAll("text.link")
         .data(graph.links)
         .enter().append("text")
         .attr("class", "link")
-        .attr("x", (d) => this.centerTextX(d.source.x, d.target.x, d.source.y, d.target.y))
-        .attr("y", (d) => this.centerTextY(d.source.x, d.target.x, d.source.y, d.target.y))
+        .attr("x", (d) => this.centerTextX(d.source.x, d.target.x, d.source.y, d.target.y, 10))
+        .attr("y", (d) => this.centerTextY(d.source.x, d.target.x, d.source.y, d.target.y, 10))
         .attr("dy", 5)
         .attr("text-anchor", "middle")
         .text(function(d) { return d.weight });
 
-
     this.nodeWrapper = this.nodeGroup
         .enter().append("g")
-        // .attr("transform", function(d) { return `translate(${d.x},${d.y})` })
 
     this.nodeWrapper
         .append("circle")
@@ -125,17 +141,6 @@ class Visualization {
         .style("fill", "LightBlue")
         .style("stroke", "black")
         .style("stroke-width", 2);
-
-    // let render = () => {
-    //   this.links
-    //     .attr("x1", function(d) { return d.source.x; })
-    //     .attr("y1", function(d) { return d.source.y; })
-    //     .attr("x2", function(d) { return d.target.x; })
-    //     .attr("y2", function(d) { return d.target.y; })
-    // }
-
-    // this.force = d3.forceSimulation()
-    // this.force.on("tick", render);
   }
 
   unhighlightNode(id) {
@@ -162,6 +167,42 @@ class Visualization {
       .style('marker-end', 'url(#color2)')
       .style('stroke', color);
     this.colorPath.style('stroke', color).style('fill', color);
+  }
+
+  animateLink(fromId, toId, color) {
+    let link = this.links._groups[0].find( link => link.id === `${fromId}-${toId}`);
+    let source = this.nodeList[link.id[0]];
+    let target = this.nodeList[link.id[2]];
+    let path = this.svg.append("path")
+      .attr("stroke", "red")
+      .attr("class", "link")
+      .attr("fill", "none")
+      .attr("d", (d) => this.bezierLine([
+          [source.x, source.y],
+          [source.x, source.y]
+        ])
+      )
+      .style("stroke-width", 3);
+
+    path
+      .transition()
+      .duration(1000)
+      .attr("d", this.bezierLine([
+        [source.x, source.y],
+        [this.centerTextX(source.x, target.x, source.y, target.y, 40), this.centerTextY(source.x, target.x, source.y, target.y, 40)],
+        [target.x, target.y]
+      ]))
+      .attrTween("stroke-dasharray", function() {
+            d3.select(this).moveToBack();
+            var len = Math.sqrt((source.x - target.x) ** 2 + (source.y - target.y) ** 2);
+            return function(t) { return (d3.interpolateString("0," + len, len + ",0"))(t) };
+      })
+      .style("marker-end", "url(#arrow)")
+    setTimeout( () => this.arrowPath.transition().duration(300).style("opacity", "1"), 500 );
+    setTimeout( () => {
+      this.arrowPath.transition().duration(700).style("opacity", "0");
+      path.transition().duration(500).style("opacity", "0").remove();
+    }, 1500)
   }
 
   changeText(textFunction) {
