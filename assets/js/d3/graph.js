3791d75b2d82f77d7d01dc6d489dd9390728da60
ranellereyes <ranelle.reyes@gmail.com>
2017-06-08 13:25:42 -0700

    Fix graph

--- assets/js/d3/graph.js
+++ assets/js/d3/graph.js
@@ -2,31 +2,31 @@ class Graph {
 
   draw() {
     let data = [
-      {numNodes: 10, runtime: 100},
-      {numNodes: 50, runtime: 200},
-      {numNodes: 100, runtime: 300},
-      {numNodes: 500, runtime: 400},
-      {numNodes: 1000, runtime: 500}
+      {numNodes: 10, runtime: 20},
+      {numNodes: 50, runtime: 100},
+      {numNodes: 100, runtime: 200},
+      {numNodes: 500, runtime: 1000},
+      {numNodes: 1000, runtime: 2000}
     ];
 
     var div = d3.select("div.comp-graph"),
         svg = div.append("svg"),
         margin = {top: 20, right: 20, bottom: 30, left: 50},
-        // width = Number(svg.attr("width")) - margin.left - margin.right,
-        // height = Number(svg.attr("height")) - margin.top - margin.bottom,
+        // width = Number(div.attr("width")) - margin.left - margin.right,
+        // height = Number(div.attr("height")) - margin.top - margin.bottom,
         width = 500,
         height = 500,
         g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        debugger;
-    // var parseTime = d3.timeParse("%d-%b-%y");
     svg.data(data);
+    svg.style("width", width)
+        .style("height", height);
 
     var x = d3.scaleLinear()
-        .rangeRound([0, width]);
+        .rangeRound([0, width - margin.left - margin.right]);
 
     var y = d3.scaleLinear()
-        .rangeRound([height, 0]);
+        .rangeRound([height - margin.bottom - margin.top, 0]);
 
     var line = d3.line()
         .x(function(d) { return x(d.numNodes); })
@@ -43,10 +43,10 @@ class Graph {
       y.domain(d3.extent(data, function(d) { return d.runtime; }));
 
       g.append("g")
-          .attr("transform", "translate(0," + height + ")")
-          .call(d3.axisBottom(x))
-        .select(".domain")
-          .remove();
+          .attr("transform", "translate(0," + 450 + ")")
+          .call(d3.axisBottom(x));
+        // .select(".domain")
+        //   .remove();
 
       g.append("g")
           .call(d3.axisLeft(y))
@@ -67,80 +67,5 @@ class Graph {
           .attr("stroke-width", 1.5)
           .attr("d", line);
   }
-  // draw() {
-  //
-  // // Width = Flex, height = 500px
-  // // Set the dimensions of the canvas / graph
-  // var margin = {top: 30, right: 20, bottom: 30, left: 50},
-  //     width = 500,
-  //     height = 500;
-  //
-  // // // Parse the date / time
-  // // var parseDate = d3.time.format("%d-%b-%y").parse;
-  //
-  // // debugger;
-  // // Set the ranges
-  // var x = d3.range([0, width]);
-  // var y = d3.range([height, 0]);
-  //
-  // // Adds the svg canvas
-  // var svg = d3.select("li.comp-graph")
-  //   .append("svg")
-  //   .attr("width", width + margin.left + margin.right)
-  //   .attr("height", height + margin.top + margin.bottom)
-  //   .append("g")
-  //   .attr("transform",
-  //   "translate(" + margin.left + "," + margin.top + ")");
-  //
-  // // Define the axes
-  // var xAxis = d3.select(".axis")
-  //     .call(d3.axisBottom(x)).ticks(5);
-  //
-  // var yAxis = d3.select(".axis")
-  //     .call(d3.axisLeft(y)).ticks(5);
-  //
-  // // Define the line
-  // var valueline = d3.svg.line()
-  //     .x(function(d) { return x(d.numNodes); })
-  //     .y(function(d) { return y(d.runtime); });
-  //
-  //
-  // // Get the data
-  //   d3.data = [
-  //               {numNodes: 10, runtime: 100},
-  //               {numNodes: 50, runtime: 200},
-  //               {numNodes: 100, runtime: 300},
-  //               {numNodes: 500, runtime: 400},
-  //               {numNodes: 1000, runtime: 500}
-  //             ];
-  //
-  //     // d3.csv("data.csv", function(error, data) {
-  //     //     data.forEach(function(d) {
-  //     //         d.date = parseDate(d.date);
-  //     //         d.close = Number(d.close);
-  //     //    }
-  //     // });
-  //
-  //     // Scale the range of the data
-  //     x.domain(d3.extent(data, function(d) { return d.numNodes; }));
-  //     y.domain([0, d3.max(data, function(d) { return d.runtime; })]);
-  //
-  //     // Add the valueline path.
-  //     svg.append("path")
-  //         .attr("class", "line")
-  //         .attr("d", valueline(data));
-  //
-  //     // Add the X Axis
-  //     svg.append("g")
-  //         .attr("class", "x axis")
-  //         .attr("transform", "translate(0," + height + ")")
-  //         .call(xAxis);
-  //
-  //     // Add the Y Axis
-  //     svg.append("g")
-  //         .attr("class", "y axis")
-  //         .call(yAxis);
-  // }
-}
-
+  
 export default Graph;
