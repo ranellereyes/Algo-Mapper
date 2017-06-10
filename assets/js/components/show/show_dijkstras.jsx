6c609dc9463a996cf1884cc310406a6f03d6bf74
bbchui <%= GitConfig.user_email %>
2017-06-10 16:41:53 -0700

    add and edit dijstras display code

--- assets/js/components/show/show_dijkstras.jsx
+++ assets/js/components/show/show_dijkstras.jsx
@@ -20,6 +20,20 @@ class ShowDijkstras extends React.Component {
     visual.draw();
     this.setState({ graph: visual });
     this.algorithm = new DijkstraSteps(NODELIST, 1, 6, visual);
+    this.fetchCode('static/javascript/dijkstras.js');
+  }
+
+  fetchCode(file) {
+    var f = new XMLHttpRequest();
+    f.open("GET", file, false);
+    f.onreadystatechange = () => {
+      if(f.readyState === 4) {
+        if(f.status === 200 || f.status == 0) {
+          this.code = f.responseText;
+        }
+      }
+    };
+    f.send(null);
   }
 
   componentWillUnmount() {
@@ -61,70 +75,7 @@ class ShowDijkstras extends React.Component {
               <div className="visualization" />
               <aside className="show-code">
                   <Highlight class="javascript-snippet">
-{`class Dijkstra {
-  constructor(nodeList) {
-    this.nodeList = nodeList;
-    this.visited = [];
-    this.unvisited = [];
-  }
-
-  initiate(source) {
-    source.weight = 0;
-    source.cost = 0;
-    this.unvisited.push(source);
-    Object.keys(this.nodeList).forEach(id => {
-      let node = this.nodeList[id];
-      if (node !== source) {
-        node.cost = Infinity;
-        this.unvisited.push(node);
-      }
-    });
-  }
-
-  search(source, destination) {
-    this.initiate(this.nodeList[source])
-    let parent = {};
-    let node = this.nodeList[source];
-    while (this.unvisited.length !== 0) {
-      node.children.sort((a,b) => a.weight - b.weight).forEach(child => {
-        let _node;
-        if (this.unvisited.indexOf(this.nodeList[child.id]) !== -1) {
-          _node = this.nodeList[child.id];
-          if (_node.cost > node.cost + child.weight) {
-            _node.cost = node.cost + child.weight;
-            parent[_node.id] = node.id;
-          }
-        }
-      });
-      this.visited.push(node);
-      this.unvisited.splice(this.unvisited.indexOf(node), 1);
-
-
-      node = this.unvisited[0]
-      for (let i = 1; i < this.unvisited.length; i++) {
-        if (this.unvisited[i].cost < node.cost) {
-          node = this.unvisited[i];
-        }
-      }
-    }
-    this.createPath(parent, this.nodeList[source], this.nodeList[destination]);
-  }
-
-  createPath(parent, source, destination) {
-    let path = [destination.id];
-    let startKey = destination.id;
-    while (parent[startKey]) {
-      path.push(parent[startKey]);
-      startKey = parent[startKey]
-    }
-    return path.reverse();
-  }
-
-
-}
-
-export default Dijkstra;
-`}
+                    {this.code}
                   </Highlight>
               </aside>
               <figure onClick={this.handleClickLeft} id="arrow_left"></figure>
@@ -138,14 +89,20 @@ export default Dijkstra;
             <ul className="show-algo-description">
               <div className="show-how-it-works">
                 <h3>How it Works</h3>
-                  Breadth-first Search
-                  <ol>
-                    <p>Greedily finds the cost from a source node to a destination node or all other nodes within a graph that does not contain negative edge(s). </p>
-                    <li>1. Assign a source node and set its cost to 0. Set all other nodes' cost to infinity.</li>
-                    <li>2. Set source node to current node and mark all nodes as unvisisted.</li>
-                    <li>3. Find all unvisisted neighbors (children) of current node and calculates the tentative cost to each neighbor. If the tentative cost to the neighbor is less than the neighbor's current cost, set its cost to the tentative cost.</li>
-                    <li>4. After the current node checks all of its unvisited neighbors, mark the current node as visited. Visited nodes will never be checked again.</li>
-                    <li>5. If all nodes have been visited or the destination node as been visited, the algorithm is finished and stops. Else, the current node is set to the node with the lowest cost and return back to step 3.</li>
+                <p>
+                Dijkstra’s (pronounced ‘DYKE-struh’) algorithm is an algorithm that finds the <code>cost</code> (shortest path) from a source node to a destination node or all other nodes within a graph that does not contain negative edge weights. Edge weights are numerical values that represent the <code>cost</code> of getting from one node to another connecting node.</p>
+                    <ol>
+                      <li><span>Assign a source node and set its <code>cost</code> to 0. Set all other nodes' <code>cost</code> to infinity.</span></li>
+                      <li><span>Set source node to current node and mark all nodes as unvisisted.</span></li>
+                      <li><span>Find all unvisisted neighbors (children) of current node and calculates the tentative <code>cost</code> to each neighbor. If the tentative <code>cost</code> to the neighbor is less than the neighbor's current <code>cost</code>, set its <code>cost</code> to the tentative <code>cost</code>.</span></li>
+                        <ul className='how-it-works'>
+                        	<li><span><code>f(n) = g(n) + h(n)</code></span></li>
+                        	<li><span><code>g</code> is the cost to reach a node, in this case dictated by the weight of an edge (the link between parent and child node)</span></li>
+                        	<li><span><code>h</code> is defined by an arbitrary calculation based on the distance from the end node</span></li>
+                        	<li><span><code>f</code> is equal to the sum of these two values</span></li>
+                        </ul>
+                      <li><span>After the current node checks all of its unvisited neighbors, mark the current node as visited. Visited nodes will never be checked again.</span></li>
+                      <li><span>If all nodes have been visited or the destination node as been visited, the algorithm is finished and stops. Else, the current node is set to the node with the lowest <code>cost</code> and return back to step 3.</span></li>
                   </ol>
                 <h3>Math</h3>
                 <p>
