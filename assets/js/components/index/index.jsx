472d54f4c38b6b86abaa355f9146882538673b28
Janice Yura Lee <JuraAir@JuraAir.local>
2017-06-10 22:51:19 -0700

    Add linkedin and github link

--- assets/js/components/index/index.jsx
+++ assets/js/components/index/index.jsx
@@ -85,24 +85,40 @@ class Index extends React.Component {
         <footer>
           <ul className="author-info">
             <li>
-              <figure className="github"></figure>
-              <figure className="linkedin"></figure>
-              <span>Ranelle Reyes</span>
+              <a href="https://github.com/bbchui">
+                <figure className="github"></figure>
+              </a>
+              <a href="https://www.linkedin.com/in/brandon-chui">
+                <figure className="linkedin"></figure>
+              </a>
+              <span>Brandon Chui</span>
             </li>
             <li>
-              <figure className="github"></figure>
-              <figure className="linkedin"></figure>
-              <span>Winber Xu</span>
-            </li>
-            <li>
-              <figure className="github"></figure>
-              <figure className="linkedin"></figure>
+              <a href="https://github.com/JaniceYR">
+                <figure className="github"></figure>
+              </a>
+              <a href="https://www.linkedin.com/in/janiceyuralee/">
+                <figure className="linkedin"></figure>
+              </a>
               <span>Janice Yura Lee</span>
             </li>
             <li>
-              <figure className="github"></figure>
-              <figure className="linkedin"></figure>
-              <span>Brandon Chui</span>
+              <a href="https://github.com/ranellereyes/">
+                <figure className="github"></figure>
+              </a>
+              <a href="https://www.linkedin.com/in/ranellereyes">
+                <figure className="linkedin"></figure>
+              </a>
+              <span>Ranelle Reyes</span>
+            </li>
+            <li>
+              <a href="https://github.com/winber2">
+                <figure className="github"></figure>
+              </a>
+              <a href="https://www.linkedin.com/in/winber/">
+                <figure className="linkedin"></figure>
+              </a>
+              <span>Winber Xu</span>
             </li>
           </ul>
         </footer>
