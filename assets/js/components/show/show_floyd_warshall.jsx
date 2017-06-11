059af5fd524d9370fb28dea15a81f27bb9d96929
bbchui <%= GitConfig.user_email %>
2017-06-11 15:05:17 -0700

    adding description to floyd-warshall

--- assets/js/components/show/show_floyd_warshall.jsx
+++ assets/js/components/show/show_floyd_warshall.jsx
@@ -78,100 +78,7 @@ class ShowFloyd extends React.Component {
               <div className="visualization" />
               <aside className="show-code">
                 <Highlight class="javascript-snippet">
-{`class FloydWarshall {
-  constructor (nodelist) {
-    this.nodelist = nodelist;
-    this.indices = Object.keys(nodelist);
-    this.costs = Object.keys(nodelist).map(e => new Array);
-    this.parents = Object.keys(nodelist).map(e => new Array);
-
-    this.initCosts();
-    this.initParents();
-  }
-
-  initCosts () {
-    this.costs.forEach((row, i) => {
-      this.costs.forEach((ele, j) => {
-        if (i === j) {
-          this.costs[i][j] = 0;
-        } else {
-          this.costs[i][j] = Infinity;
-        }
-      });
-    });
-
-    Object.keys(this.nodelist).forEach(nodeId => {
-      this.nodelist[nodeId].children.forEach(child => {
-        let nodeIdx = this.indices.indexOf(nodeId),
-            childIdx = this.indices.indexOf(String(child.id));
-        this.costs[nodeIdx][childIdx] = child.weight;
-      });
-    });
-  }
-
-  initParents () {
-    Object.keys(this.nodelist).forEach(nodeId => {
-      this.nodelist[nodeId].children.forEach(child => {
-        let nodeIdx = this.indices.indexOf(nodeId),
-            childIdx = this.indices.indexOf(String(child.id));
-
-        this.parents[nodeIdx][childIdx] = nodeId;
-      });
-    });
-
-    for (let i = 0; i < this.parents.length; i++) {
-      for (let j = 0; j < this.parents.length; j++) {
-        if (i === j) {
-          this.parents[i][j] = null;
-        } else if (!this.parents[i][j]) {
-          this.parents[i][j] = undefined;
-        }
-      }
-    }
-  }
-
-  pathDeconstructor(start, end, intermediate = null) {
-    let path = [end],
-        startIdx = this.indices.indexOf(start),
-        endIdx = this.indices.indexOf(end),
-        intIdx = this.indices.indexOf(intermediate);
-
-    if (intIdx > -1) {
-      while (intermediate !== path[0]) {
-        let parent = this.parents[intIdx][this.indices.indexOf(path[0])];
-        if (!parent) { break; }
-        path.unshift(parent);
-      }
-    }
-
-    while (start !== path[0]) {
-      let parent = this.parents[startIdx][this.indices.indexOf(path[0])];
-      if (!parent) {
-        path.unshift(start);
-        break;
-      }
-      path.unshift(parent);
-    }
-
-    return path;
-  }
-
-  search (start, end) {
-    for (let k = 0; k < this.indices.length; k++) {
-      for (let i = 0; i < this.indices.length; i++) {
-        for (let j = 0; j < this.indices.length; j++) {
-          if (this.costs[i][j] > this.costs[i][k] + this.costs[k][j]) {
-            this.costs[i][j] = this.costs[i][k] + this.costs[k][j];
-            this.parents[i][j] = this.parents[k][j];
-          }
-        }
-      }
-    }
-
-    return this.pathDeconstructor(start, end);
-  }
-}
-`}
+                  {this.code}
                 </Highlight>
               </aside>
               <figure onClick={this.handleClickLeft}></figure>
@@ -185,7 +92,8 @@ class ShowFloyd extends React.Component {
             <ul className="show-algo-description">
               <div className="show-how-it-works">
                 <h3>How it Works</h3>
-                <p>asdfaf  EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.
+                <p>
+                  
                 </p>
                 <h3>Math</h3>
                 <p>asdfasdf  EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.EXTREMELY IMPORTANT DESCRIPTION.
